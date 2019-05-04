05bff2b88cb2b4888c3bccdfb78303e99ccd949c
rlewis030 <rlewis030@gmail.com>
2019-05-04 12:04:59 -0400

    Initial Upload
    Start of development

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// Bootstrap JS
+import 'bootstrap';
+import './scss/app.scss';
+
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+        .then(() => {
+            console.log('Service Worker Registered!');
+        })
+}
