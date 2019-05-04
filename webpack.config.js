8307d6c4e7719d36c7bb4b7b04873656d5ff07b0
rlewis030 <rlewis030@gmail.com>
2019-05-04 13:51:14 -0400

    Service Worker Fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'js')
+        path: path.resolve(__dirname, 'dist/js')
     },
     module: {
         rules: [
